b13c7b5e2368e899878e3c33722466f1a77b364f
Shay <shay@acorngroves.co.uk>
2018-08-21 15:49:29 +0100

    Cleanup. And telling you NOT to bug TripodGG about this script.

--- rec-tripodgg.ps1
+++ rec-tripodgg.ps1
@@ -1,4 +1,10 @@
-﻿# I wrote this while high as hell on caffeine and nicotine. Fingers wont stop shaking.
+﻿# This has been modified specifically for TripodGG's community. No other uses allowed.
+# Will only capture his stream unless modified. I don't do support but join his discord
+# https://discord.gg/NWJS2tg
+# And bug RetroBecky about it.
+
+
+# I wrote this while high as hell on caffeine and nicotine. Fingers wont stop shaking.
 # I tried to clean syntax and spelling as I wrote it, errors may occur.
 
 # Expects Streamlink installed and added to the $PATH
@@ -7,7 +13,7 @@ $EndTime = Read-Host "Enter a local time to end capture (ex: 12:05 PM): "
 $RND = [System.IO.Path]::GetRandomFileName()
 
 # If TripodGG is offline then it will capture anything he hosts. Go bug him to get his ass online on
-# his discord: https://discord.gg/NWJS2tg
+# his discord.
 
 Measure-Command {
 
@@ -33,11 +39,3 @@ Wait-Process -Id $pyid # Wait for clean exit()
 }
 
 Write-Host "Process stopped. Stream captured. Recording should be located at: TripodGG_$RND.mp4" # Return(1) (yes I'm a C hacker) message.
-
-# ~/.Trash - Didn't need any of the below lines, so just dropped them here.
-
-#$LogTarget = Read-Host "Enter name for log file: "
-#$args = "https://www.twitch.tv/$Target best -o $RecTarget.mp4"
-#$Proc = streamlink -LoadUserProfile -ArgumentList "https://www.twitch.tv/$Target best -o $RecTarget.mp4" -PassThru
-# Wait-Process -Id $steid # Wait for clean exit()
-#iex $Proc
